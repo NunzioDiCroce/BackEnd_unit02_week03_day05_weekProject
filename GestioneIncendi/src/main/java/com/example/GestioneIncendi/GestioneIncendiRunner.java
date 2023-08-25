e87179b32c68f8b9c426313429633c3d5c295746
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-25 12:24:16 +0200

    Create GestioneIncendiRunner.java

---
+++ GestioneIncendi/src/main/java/com/example/GestioneIncendi/GestioneIncendiRunner.java
@@ -0,0 +1,15 @@
+package com.example.GestioneIncendi;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GestioneIncendiRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
