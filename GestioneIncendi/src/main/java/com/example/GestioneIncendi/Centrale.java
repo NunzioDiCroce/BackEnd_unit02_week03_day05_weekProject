46bbf80bed4426ce320b2bb1d635c620ad4ff190
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-25 15:13:04 +0200

    Create appChannel parameter

--- GestioneIncendi/src/main/java/com/example/GestioneIncendi/Centrale.java
+++ GestioneIncendi/src/main/java/com/example/GestioneIncendi/Centrale.java
@@ -1,14 +1,20 @@
 package com.example.GestioneIncendi;
 
+import org.springframework.beans.factory.annotation.Value;
+
 // This class is the Observer
 
 public class Centrale {
 
+	// Parametrizzazione canale di comunicazione sonda - centrale
+	@Value("${appChannel}")
+	private String appChannel;
+
 	public void comunicaAllarme(Sonda sonda) {
 		System.out.println("");
 		System.out.println("GESTIONE INCENDI APP >>> Messaggio di rilevazione incendio");
-		System.out.println("http://host/alarm?idsonda=" + sonda.getIdSonda() + "&lat=" + sonda.getLatitudine() + "&lon="
-				+ sonda.getLatitudine() + "&smokelevel=" + sonda.getLivelloFumo());
+		System.out.println(appChannel + "http://host/alarm?idsonda=" + sonda.getIdSonda() + "&lat="
+				+ sonda.getLatitudine() + "&lon=" + sonda.getLatitudine() + "&smokelevel=" + sonda.getLivelloFumo());
 	}
 
 }
